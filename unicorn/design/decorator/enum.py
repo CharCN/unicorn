456746811348690458c29f4d3b5e88244b25abfe
magus0219 <magus0219@gmail.com>
2014-07-07 16:49:54 +0800

    Revert 92ad14c..4064d4a
    This rolls back to commit 92ad14c3f90d18d79246d4dfa56fbde9cbfff0d4.

--- unicorn/design/decorator/enum.py
+++ unicorn/design/decorator/enum.py
@@ -42,7 +42,6 @@ class EnumMetaClass(type):
     def __contains__(self,item):
         return item.upper() in self.__enum_strs__
 
-print 123222222
 def enum(cls):
     # Just wrapper class using EnumMetaClass 
     cls = EnumMetaClass(cls.__name__,cls.__bases__,cls.__dict__)
